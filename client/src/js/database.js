714e847938b330e0657f1227d44ead730eed7e70
AliyusUnderwood <158104889+AliyusUnderwood@users.noreply.github.com>
2024-07-16 16:34:00 -0700

    Pasted starter code and finished all todos

---
+++ client/src/js/database.js
@@ -0,0 +1,44 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Add content to the database
+export const putDb = async (content) => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ content });
+    const result = await request;
+    console.log('Data saved to the database', result);
+  } catch (error) {
+    console.error('putDb not implemented', error);
+  }
+};
+
+// Get all content from the database
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('Data retrieved from the database', result);
+    return result;
+  } catch (error) {
+    console.error('getDb not implemented', error);
+  }
+};
+
+initdb();
